fa1ec70063231612827fdb3e1eff798381cae0aa
Mario Zito <mario.zito@treme.io>
2024-06-28 14:45:43 -0300

    Refactor application handlers

---
+++ src/voting/handlers.ts
@@ -0,0 +1,57 @@
+/**
+ * All application handlers go here ...
+ */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Response } from "../semaphore/index.js";
+import { selectElectors } from "./selection.js";
+import { assignTasks, getAssignedTasks } from "./assignments.js";
+
+
+export async function assignElectorsHandler(
+  data: any
+): Promise<Response> {
+  /** @throw any errors thrown here will be catched by the dispatcher */
+  let { communityUid, claims, strategy } = data;
+
+  // first select the electors for each claim 
+  // based on the given strategy
+  let rsp = await selectElectors({
+    communityUid: communityUid,
+    planStrategy: strategy,
+    claims: claims
+  })
+  if (!rsp.success) return {
+    success: false,
+    data: null,
+    error: `Could not select electors for plan '${strategy.planUid}', error: `+
+      (rsp.error.message || rsp.error)
+  }
+  
+  // now create the tasks list for each elector
+  // the list is stored in KVS an can be retrieved by each elector
+  let assignedClaims = (rsp.data as any).claims;
+  let errors =  (rsp.data as any).errors;
+
+  await assignTasks(strategy.planUid, assignedClaims);
+  return {
+    success: true, error: null,
+    data: { claims: assignedClaims, errors: errors }
+  }
+}
+
+
+/** @throw any errors thrown here will be catched by the dispatcher */
+export async function retrieveAssignmentsHandler(
+  data: any
+): Promise<Response> {
+  let { identityCommitment, ownershipProof } = data;
+
+  let assigned = await getAssignedTasks(
+    identityCommitment,
+    ownershipProof
+  );
+  return {
+    success: true, error: null,
+    data: assigned
+  }
+}
--- src/voting/main.ts
+++ src/voting/main.ts
@@ -1,50 +1,13 @@
-import { Response } from "../semaphore/index.js";
 import { registerApplicationHandler } from "../services/dispatcher.js";
 import { startListener } from "../services/listener.js"
-import { selectElectors } from "./selection.js";
-import { assignTasks, getAssignedTasks } from "./assignments.js";
+import { 
+  assignElectorsHandler, 
+  retrieveAssignmentsHandler 
+} from "./handlers.js";
 
+// we need to register  all application handlers 
+registerApplicationHandler('assignElectors', assignElectorsHandler);
+registerApplicationHandler('retrieveAssignments', retrieveAssignmentsHandler)
 
-registerApplicationHandler('selectElectors', async (data: any): Promise<Response> => {
-  let { communityUid, claims, strategy } = data;
-
-  // first select the electors for each claim 
-  // based on the given strategy
-  let rsp = await selectElectors({
-    communityUid: communityUid,
-    planStrategy: strategy,
-    claims: claims
-  })
-  if (!rsp.success) return {
-    success: false,
-    data: null,
-    error: `Could not select electors for plan '${strategy.planUid}', error: `+
-      (rsp.error.message || rsp.error)
-  }
-  
-  // now create the tasks list for each elector
-  // the list is stored in KVS an can be retrieved by each elector
-  let assignedClaims = (rsp.data as any).claims;
-  let errors =  (rsp.data as any).errors;
-
-  await assignTasks(strategy.planUid, assignedClaims);
-  return {
-    success: true, error: null,
-    data: { claims: assignedClaims, errors: errors }
-  }
-});
-
-
-registerApplicationHandler('getAssignments', async (data: any): Promise<Response> => {
-  let { identityCommitment } = data;
-  
-  let assigned = await getAssignedTasks(identityCommitment);
-  return {
-    success: true, error: null,
-    data: { assigned }
-  }
-});
-
-
-// Start the NATSClient
+// start the NATSClient and we are running !
 startListener();
