697a7b1ffaa20c14a7d349924e206ea3d67f1188
Mario Zito <mario.zito@treme.io>
2024-07-10 17:59:25 -0300

    Graphql for getting txn status does not work anymore !

--- test/graphql-call-zeko.test.ts
+++ test/graphql-call-zeko.test.ts
@@ -2,20 +2,60 @@ import axios from "axios";
 
 describe("Zeko Graphql tests", () => {
   
-  const endpoint = "https://devnet.zeko.io/graphql";
+  const GRAPHQL_ENDPOINT = "https://proxy.devnet.minaexplorer.com/graphql";
+  // #"https://proxy.devnet.minaexplorer.com/graphql"; // "https://api.minascan.io/node/devnet/v1/graphql"; // "https://devnet.zeko.io/graphql";
 
-  it(`should get the balance`, async () => {
-    const txnId = "5Juah5iEhppYhopNLHtKKVPVWJ8Yc6RTCo2s2K65Uezucng9DgzF"
-    const response = await axios.post(endpoint, {
-      query: `{\n  zkapp(query: {hash: "${txnId}"}) {
+  it(`Should get the Txn`, async () => {
+    const txnId = "5Jtm1AThF1qT7qqxPQoQyyP1ZpDcNe4G4L9Y8eKnUmXqhfXcftom";
+    await queryTxnStatus(GRAPHQL_ENDPOINT, txnId);
+  });
+});
+
+async function queryTxnStatus(url: string, txnId: string): Promise<any> {
+  try {
+    let payload = JSON.stringify({
+      "query": `{\n  zkapp(query: {hash: \"${txnId}\"}) {
         blockHeight
         failureReason {
           failures
           index
         }    
       }\n}`,
-      variables: null,
-    });
-    console.log("response:", response.data, response.data.errors);
-  });
-});
+      "variables":null
+    });    
+    // let payload = {"query": "{ bestChain(maxLength: 10) { creator }}"};
+    console.log(`payload=`, payload);
+
+    const response = await axios.post(url, 
+      payload, 
+      { headers: { 'content-type': 'application/json' } }
+    );
+    // expected 
+    /*
+    "data": {
+      "data": {
+        "zkapp": {
+          "blockHeight": 6896,
+          "failureReason": null
+        }
+      }
+    },
+    "error": null
+    */
+    console.log("axios response=", JSON.stringify(response.data, null, 2));
+   
+    const answer = response.data?.data?.zkapp;
+    return {
+      data: answer,
+      error: null,
+    };
+  } catch (err: any) {
+    console.log("queryTxnStatus err=", err);
+    return {
+      data: null,
+      error: err,
+    };
+  }
+
+  return true;
+}
