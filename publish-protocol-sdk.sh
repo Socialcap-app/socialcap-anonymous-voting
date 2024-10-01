1c5b985a7fc227c3aca6ce1bfe89277d992ee84b
Mario Zito <mario.zito@treme.io>
2024-10-01 02:32:57 -0300

    Prepara pkg for protocol-sdk and publish

--- publish-protocol-sdk.sh
+++ publish-protocol-sdk.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Publish to NPM
 pnpm build
-cd src/pkg
+
+# Move to pkg folder
+cd pkg
+
+# Copy needed files to pkg
+cp ../build/src/semaphore/* ./dist
+ls -al 
+
+# Publish to NPM
 pnpm publish --access public --no-git-checks
--- src/pkg/README.md
+++
@@ -1,8 +0,0 @@
-# NPM Package bundler
-
-It includes ONLY the files that need to be published to NPM (semaphore).
-
-It exposes the SDK, BUT not all protocol files.
-
-
-
